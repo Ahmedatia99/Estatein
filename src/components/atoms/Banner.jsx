e7982312230663c1f502f2f01c4f2398799e438a
Ahmedatia99 <atyaa629@gmail.com>
2024-05-01 23:32:31 +0300

    add navbar

--- src/components/atoms/Banner.jsx
+++ src/components/atoms/Banner.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function Banner() {
   return (
     <main className=" container mx-auto bg-banner-img bg-cover bg-junior text-white">
-      <section className="flex justify-center items-center">
+      <section className="flex justify-center items-center p-3">
         <h2 className="text-base font-medium">
           ✨Discover Your Dream Property with Estatein
         </h2>
-        <a href="">Learn more</a>
+        <a href="" className="p-0 pl-2 underline">
+          Learn more
+        </a>
       </section>
     </main>
   );
