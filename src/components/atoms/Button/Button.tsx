ec97cc97e58113fbac3cb1dfdf90c536c8366344
Ahmedatia99 <atyaa629@gmail.com>
2024-05-02 02:40:23 +0300

    additions in  navbar

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HTMLAttributes } from "react";
+import classNames from "classnames";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "second ";
+  action: string;
+  displays?: "true" | "false";
+  paddingname?: string;
+}
+
+export const Button = ({
+  action,
+  variant = "primary",
+  paddingname,
+}: ButtonProps) => {
+  const variants = {
+    primary: " bg-purple text-white",
+    second: "bg-senior  text-white ",
+    greedy: "bg-litewhite text-textcook",
+  }[variant];
+
+  return (
+    <button
+      className={classNames(
+        `flex items-center justify-center rounded-lg transition-all text-sm mr-3`,
+        variants,
+        paddingname
+      )}
+    >
+      {action}
+    </button>
+  );
+};
