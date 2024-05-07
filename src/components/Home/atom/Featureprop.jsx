b96531bf87bf23b7b3fa1b94bfb4d65b8f2ef768
Ahmedatia99 <atyaa629@gmail.com>
2024-05-07 23:58:12 +0300

    additions feature section in headers

--- src/components/Home/atom/Featureprop.jsx
+++ src/components/Home/atom/Featureprop.jsx
@@ -10,19 +10,19 @@ export default function Featureprop() {
       <section className=" border-stroke border-2 p-3 grid grid-cols-4 gap-3 ">
         <div className="py-8 px-4 bg-junior rounded-xl flex flex-col justify-center items-center">
           <div className="rounded-full border-purple border-2 border-r-senior border-l-senior w-fit mb-6">
-            <HiHome className="text-5xl p-3" />
+            <HiHome className="text-5xl p-3 text-purple" />
           </div>
           <spna className="text-base font-semibold">Find Your Dream Home</spna>
         </div>
         <div className="py-8 px-4 bg-junior rounded-xl flex flex-col justify-center items-center">
           <div className="rounded-full border-purple border-2 border-r-senior border-l-senior w-fit mb-6 ">
-            <IoCamera className="text-5xl p-3" />
+            <IoCamera className="text-5xl p-3 text-purple" />
           </div>
           <spna className="text-base font-semibold">Unlock Property Value</spna>
         </div>
         <div className="py-8 px-4 bg-junior rounded-xl flex flex-col justify-center items-center">
           <div className="rounded-full border-purple border-2 border-r-senior border-l-senior w-fit mb-6 ">
-            <HiMiniBuildingOffice2 className="text-5xl p-3" />
+            <HiMiniBuildingOffice2 className="text-5xl p-3 text-purple" />
           </div>
           <spna className="text-base font-semibold">
             Effortless Property Management
@@ -30,7 +30,7 @@ export default function Featureprop() {
         </div>
         <div className="py-8 px-4 bg-junior rounded-xl flex flex-col justify-center items-center">
           <div className="rounded-full border-purple border-2 border-r-senior border-l-senior w-fit mb-6 ">
-            <IoSunny className="text-5xl p-3" />
+            <IoSunny className="text-5xl p-3 text-purple" />
           </div>
           <spna className="text-base font-semibold">
             Smart Investments, Informed Decisions
