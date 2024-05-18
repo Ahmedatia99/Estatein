d3200da7e411ade65cb9441d7c150ac13f9a1e77
Ahmedatia99 <atyaa629@gmail.com>
2024-05-18 14:16:35 +0300

    additions in feature

--- src/components/Home/HomeLayout.jsx
+++ src/components/Home/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./atom/Header";
 import Featureprop from "./atom/Featureprop"
+import Feature from "./atom/Feature"
 export default function HomeLayout() {
   return (
     <main>
       <section className="px-5">
         <Header />
         <Featureprop />
+        <Feature />
       </section>
     </main>
   );
