57d8418eafa44f0360fee059a44c52ed265369fb
Ahmedatia99 <atyaa629@gmail.com>
2024-10-20 02:09:29 +0300

    add carousl + swiper package

--- src/components/atoms/HotelCard.jsx
+++ src/components/atoms/HotelCard.jsx
@@ -4,39 +4,50 @@ import { IoBed } from "react-icons/io5";
 import { FaBath } from "react-icons/fa";
 import { HiMiniBuildingOffice } from "react-icons/hi2";
 import { Button } from "./Button/Button.tsx";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
 
 export default function HotelCard() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   };
+
   return (
-    <section>
-      <Slider {...settings}>
+    <section className="container mx-auto">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
         {data.map((el) => {
           return (
-            <div className="p-8 border-2 border-stroke rounded-xl" key={el.id}>
+            <SwiperSlide
+              className="p-8 border-2  border-stroke rounded-xl"
+              key={el.id}
+            >
               <img src={el.img} className="rounded-xl" />
               <h2 className="text-xl mt-5 mb-1 font-semibold">{el.name}</h2>
               <p className="text-tasting text-base">{el.info}</p>
-              <div className="flex justify-center items-center gap-4 my-6 ">
-                <div className="py-2 px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
+              <div className="flex justify-around items-center flex-wrap my-6 ">
+                <div className="py-2 tablet:px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
                   <IoBed />
                   <span className="ml-1">{el.bedroom}-Bedroom</span>
                 </div>
-                <div className="py-2 px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
+                <div className="py-2 tablet:px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
                   <FaBath />
                   <span className="ml-1">{el.bathroom}-Bathroom</span>
                 </div>
-                <div className="py-2 px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
+                <div className="py-2 tablet:px-4 border-2 border-stroke bg-senior rounded-3xl flex items-center">
                   <HiMiniBuildingOffice />
                   <span className="ml-1">Villa</span>
                 </div>
@@ -52,10 +63,10 @@ export default function HotelCard() {
                   variant="primary"
                 />
               </div>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </Slider>
+      </Swiper>
     </section>
   );
 }
