01fe2d800f1215ce9c165b24010b6d8ddd730b3b
Ahmedatia99 <atyaa629@gmail.com>
2024-10-29 03:03:29 +0300

    add banner image

--- src/components/atoms/Banner.jsx
+++ src/components/atoms/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {Element} from "react-scroll";
+import "../../assets/style/main.css"
 export default function Banner() {
   return (
     <Element
       name="hero"
-      className="bg-banner-img bg-cover bg-junior text-white bg-opacity-90"
+      className="ban bg-cover bg-junior text-white bg-opacity-90"
     >
       <section className="container mx-auto flex justify-center items-center p-3 mobile:p-0 mobile:py-3 ">
         <h2 className="text-xl md:text-base sm:text-sm mobile:text-xs font-medium">
